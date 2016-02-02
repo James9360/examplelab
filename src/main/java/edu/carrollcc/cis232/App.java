2f71c947b05fb3b1002f8f70eaafaa1c558f7995
James Williams <james938087@gmail.com>
2016-02-01 22:39:20 -0500

    Here's my edited file.

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -4,10 +4,18 @@ package edu.carrollcc.cis232;
  * Hello world!
  *
  */
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+    	Scanner scan = new Scanner(System.in);
+    	
+    	System.out.print ("Enter name: ");
+    	String name = scan.nextLine();
+    	System.out.println();
+        System.out.printf("Hello %s!%n", name);
+        scan.close();
     }
 }
